3e1fb5649b27482436c1624cdd07c3ab864add96
xielianqin <1323229802@qq.com>
2024-11-15 21:57:38 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include ".assignment/test.h"
 
 // 比较三个数的大小
-void CompareThreeNumbers(int a, int b, int c, Result &result)
+void CompareThreeNumbers(int a, int b, int c, Result& result)
 {
-    // 在这里完成你的代码
+    if (a <= b && a <= c) {
+        result.min = a;
+        if (b <= c) {
+            result.mid = b;
+            result.max = c;
+        } else {
+            result.mid = c;
+            result.max = b;
+        }
+    } else if (b <= a && b <= c) {
+        result.min = b;
+        if (a <= c) {
+            result.mid = a;
+            result.max = c;
+        } else {
+            result.mid = c;
+            result.max = a;
+        }
+    } else {
+        result.min = c;
+        if (a <= b) {
+            result.mid = a;
+            result.max = b;
+        } else {
+            result.mid = b;
+            result.max = a;
+        }
+    }
 }
